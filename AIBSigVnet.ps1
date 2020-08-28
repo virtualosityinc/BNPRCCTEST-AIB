ccbc4effac9664cb7fbc6f1090adcd4fc6d2ac97
virtualosityinc <devante@virtualosity-inc.com>
2020-08-28 14:10:45 -0500

    Create AIBSigVnet.ps1

---
+++ AIBSigVnet.ps1
@@ -0,0 +1,221 @@
+﻿######################################################################################################
+#
+# Prerequisites
+#
+######################################################################################################
+az feature register --namespace Microsoft.VirtualMachineImages --name VirtualMachineTemplatePreview
+
+az feature show --namespace Microsoft.VirtualMachineImages --name VirtualMachineTemplatePreview | grep state
+
+# register and enable for shared image gallery
+az feature register --namespace Microsoft.Compute --name GalleryPreview
+
+# wait until it says registered
+
+# check you are registered for the providers
+az provider show -n Microsoft.VirtualMachineImages | grep registrationState
+az provider show -n Microsoft.Storage | grep registrationState
+az provider show -n Microsoft.Compute | grep registrationState
+az provider show -n Microsoft.KeyVault | grep registrationState
+
+
+
+######################################################################################################
+#
+# Step 1 : Enable Prereqs
+# Set Permissions & Create Resource Group for Image Builder Images
+#
+#######################################################################################################
+
+# Step 1: Import module
+Import-Module Az.Accounts
+
+# Step 2: get existing context
+$currentAzContext = Get-AzContext
+
+# destination image resource group
+$imageResourceGroup="aibImageSigRG"
+
+# location (see possible locations in main docs)
+$location="eastus"
+
+## if you need to change your subscription: Get-AzSubscription / Select-AzSubscription -SubscriptionName 
+
+# get subscription, this will get your current subscription
+$subscriptionID=$currentAzContext.Subscription.Id
+
+$sigGalleryName= "aibSigWVD"
+$imageDefName ="winWksimages"
+
+# additional replication region
+$replRegion2="eastus"
+
+# name of the image to be created
+$imageName="win10wvdimage01"
+
+# image distribution metadata reference name
+$runOutputName="win10ManImg02ro"
+
+# image template name
+$imageTemplateName="window10VnetTemplate01"
+
+# distribution properties object name (runOutput), i.e. this gives you the properties of the managed image on completion
+$runOutputName="winWksSigR01"
+
+# VNET properties (update to match your existing VNET, or leave as-is for demo)
+# VNET name
+$vnetName="vnet-rcctest-wvd"
+# subnet name
+$subnetName="subnet-rcctest-wvd-wvd"
+# VNET resource group name
+$vnetRgName="rg-rcctest-network-001"
+# Existing Subnet NSG Name or the demo will create it
+$nsgName="nsg-rcctest-subnet-hub-vdi"
+# NOTE! The VNET must always be in the same region as the AIB service region.
+
+
+# create resource group for image and image template resource
+New-AzResourceGroup -Name $imageResourceGroup -Location $location
+
+
+######################################################################################################
+#
+# Networking
+# Step 2: Configure Networking
+# Add NSG rule to allow the AIB deployed Azure Load Balancer to communicate with the proxy VM
+# Disable Private Service Policy on subnet
+#
+######################################################################################################
+
+Get-AzNetworkSecurityGroup -Name $nsgName -ResourceGroupName $vnetRgName  | Add-AzNetworkSecurityRuleConfig -Name AzureImageBuilderAccess -Description "Allow Image Builder Private Link Access to Proxy VM" -Access Allow -Protocol Tcp -Direction Inbound -Priority 400 -SourceAddressPrefix AzureLoadBalancer -SourcePortRange * -DestinationAddressPrefix VirtualNetwork -DestinationPortRange 60000-60001 | Set-AzNetworkSecurityGroup
+
+$virtualNetwork= Get-AzVirtualNetwork -Name $vnetName -ResourceGroupName $vnetRgName 
+   
+($virtualNetwork | Select -ExpandProperty subnets | Where-Object  {$_.Name -eq $subnetName} ).privateLinkServiceNetworkPolicies = "Disabled"  
+ 
+$virtualNetwork | Set-AzVirtualNetwork 
+
+
+######################################################################################################
+#
+# Shared Gallery
+# Step 3: Create the Shared Image Gallery
+#
+######################################################################################################
+
+# create gallery
+New-AzGallery -GalleryName $sigGalleryName -ResourceGroupName $imageResourceGroup  -Location $location
+
+# create gallery definition
+New-AzGalleryImageDefinition -GalleryName $sigGalleryName -ResourceGroupName $imageResourceGroup -Location $location -Name $imageDefName -OsState generalized -OsType Windows -Publisher 'MicrosoftWindowsDesktop' -Offer 'windows-10' -Sku '19h1-evd'
+
+
+######################################################################################################
+#
+# DevOps
+# Assign permissions and storage account
+#
+######################################################################################################
+
+# assign the image builder spn rights to inject the image into the chosen resource group
+az role assignment create --assignee cf32a0cc-373c-47c9-9156-0db11f6a6dfc --role Contributor --scope /subscriptions/$subscriptionID/resourceGroups/$imageResourceGroup
+
+# staging storage account
+$stagingStorageAcc=aibstagstor$(date +'%s')
+
+# create storage account and blob in resource group
+az storage account create -n $stagingStorageAcc -g $imageResourceGroup -l $location --sku Standard_LRS
+
+
+##############################################################################################################
+#
+# Networking
+# Step 3.1 : Modify the Example template and create role for AIB
+# Assign permissions, create user idenity and role for AIB for distributing images and connecting to network.
+#
+##############################################################################################################
+
+# This command will download and update the template with the parameters specified earlier.
+$templateUrl="https://raw.githubusercontent.com/danielsollondon/azvmimagebuilder/master/quickquickstarts/1a_Creating_a_Custom_Win_Image_on_Existing_VNET/existingVNETWindows.json"
+$templateFilePath = "existingVNETWindows.json"
+
+$aibRoleNetworkingUrl="https://raw.githubusercontent.com/danielsollondon/azvmimagebuilder/master/solutions/12_Creating_AIB_Security_Roles/aibRoleNetworking.json"
+$aibRoleNetworkingPath = "aibRoleNetworking.json"
+
+$aibRoleImageCreationUrl="https://raw.githubusercontent.com/danielsollondon/azvmimagebuilder/master/solutions/12_Creating_AIB_Security_Roles/aibRoleImageCreation.json"
+$aibRoleImageCreationPath = "aibRoleImageCreation.json"
+
+# download configs
+Invoke-WebRequest -Uri $templateUrl -OutFile $templateFilePath -UseBasicParsing
+
+Invoke-WebRequest -Uri $aibRoleNetworkingUrl -OutFile $aibRoleNetworkingPath -UseBasicParsing
+
+Invoke-WebRequest -Uri $aibRoleImageCreationUrl -OutFile $aibRoleImageCreationPath -UseBasicParsing
+
+# update AIB image config template
+((Get-Content -path $templateFilePath -Raw) -replace '<subscriptionID>',$subscriptionID) | Set-Content -Path $templateFilePath
+((Get-Content -path $templateFilePath -Raw) -replace '<rgName>',$imageResourceGroup) | Set-Content -Path $templateFilePath
+((Get-Content -path $templateFilePath -Raw) -replace '<region>',$location) | Set-Content -Path $templateFilePath
+((Get-Content -path $templateFilePath -Raw) -replace '<runOutputName>',$runOutputName) | Set-Content -Path $templateFilePath
+((Get-Content -path $templateFilePath -Raw) -replace '<imageDefName>',$imageDefName) | Set-Content -Path $templateFilePath
+((Get-Content -path $templateFilePath -Raw) -replace '<sharedImageGalName>',$sigGalleryName) | Set-Content -Path $templateFilePath
+((Get-Content -path $templateFilePath -Raw) -replace '<region1>',$location) | Set-Content -Path $templateFilePat
+
+((Get-Content -path $templateFilePath -Raw) -replace '<vnetName>',$vnetName) | Set-Content -Path $templateFilePath
+((Get-Content -path $templateFilePath -Raw) -replace '<subnetName>',$subnetName) | Set-Content -Path $templateFilePath
+((Get-Content -path $templateFilePath -Raw) -replace '<vnetRgName>',$vnetRgName) | Set-Content -Path $templateFilePath
+
+((Get-Content -path $templateFilePath -Raw) -replace '<imgBuilderId>',$idenityNameResourceId) | Set-Content -Path $templateFilePath
+
+# setup role def names, these need to be unique
+$timeInt=$(get-date -UFormat "%s")
+$imageRoleDefName="Azure Image Builder Image Def"+$timeInt
+$networkRoleDefName="Azure Image Builder Network Def"+$timeInt
+$idenityName="aibIdentity"+$timeInt
+
+# create user identity
+## Add AZ PS module to support AzUserAssignedIdentity
+Install-Module -Name Az.ManagedServiceIdentity
+
+# create identity
+New-AzUserAssignedIdentity -ResourceGroupName $imageResourceGroup -Name $idenityName
+
+$idenityNameResourceId=$(Get-AzUserAssignedIdentity -ResourceGroupName $imageResourceGroup -Name $idenityName).Id
+$idenityNamePrincipalId=$(Get-AzUserAssignedIdentity -ResourceGroupName $imageResourceGroup -Name $idenityName).PrincipalId
+
+# update template with identity
+((Get-Content -path $templateFilePath -Raw) -replace '<imgBuilderId>',$idenityNameResourceId) | Set-Content -Path $templateFilePath
+
+# update the role defintion names
+((Get-Content -path $aibRoleImageCreationPath -Raw) -replace 'Azure Image Builder Service Image Creation Role',$imageRoleDefName) | Set-Content -Path $aibRoleImageCreationPath
+((Get-Content -path $aibRoleNetworkingPath -Raw) -replace 'Azure Image Builder Service Networking Role',$networkRoleDefName) | Set-Content -Path $aibRoleNetworkingPath
+
+# update role definitions
+((Get-Content -path $aibRoleNetworkingPath -Raw) -replace '<subscriptionID>',$subscriptionID) | Set-Content -Path $aibRoleNetworkingPath
+((Get-Content -path $aibRoleNetworkingPath -Raw) -replace '<vnetRgName>',$vnetRgName) | Set-Content -Path $aibRoleNetworkingPath
+
+((Get-Content -path $aibRoleImageCreationPath -Raw) -replace '<subscriptionID>',$subscriptionID) | Set-Content -Path $aibRoleImageCreationPath
+((Get-Content -path $aibRoleImageCreationPath -Raw) -replace '<rgName>', $imageResourceGroup) | Set-Content -Path $aibRoleImageCreationPath
+
+# create role definitions from role configurations examples, this avoids granting contributor to the SPN
+New-AzRoleDefinition -InputFile  ./aibRoleImageCreation.json
+New-AzRoleDefinition -InputFile  ./aibRoleNetworking.json
+
+# grant role definition to image builder user identity
+New-AzRoleAssignment -ObjectId $idenityNamePrincipalId -RoleDefinitionName $imageRoleDefName -Scope "/subscriptions/$subscriptionID/resourceGroups/$imageResourceGroup"
+New-AzRoleAssignment -ObjectId $idenityNamePrincipalId -RoleDefinitionName $networkRoleDefName -Scope "/subscriptions/$subscriptionID/resourceGroups/$vnetRgName"
+
+
+##############################################################################################################
+#
+# Step 4 : Submit the template
+# Build the image
+#
+##############################################################################################################
+
+# note this will take minute, as validation is run (security / dependencies etc.)
+New-AzResourceGroupDeployment -ResourceGroupName $imageResourceGroup -TemplateFile $templateFilePath -api-version "2020-02-14" -imageTemplateName $imageTemplateName -svclocation $location
+
+Invoke-AzResourceAction -ResourceName $imageTemplateName -ResourceGroupName $imageResourceGroup -ResourceType Microsoft.VirtualMachineImages/imageTemplates -ApiVersion "2020-02-14" -Action Run -Force
+
+# >>>> WAITING ON IMAGE BUILD
